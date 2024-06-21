eb7e7ad1c6e631b4f35007391215f6552bb508fb
hiddevb <hiddevanbavel@hotmail.com>
2024-06-21 13:46:55 +0200

    update files

--- src/components/cta.js
+++ src/components/cta.js
@@ -1,13 +1,13 @@
-import * as React from 'react'
+import * as React from 'react';
 
-import * as styles from './cta.module.css'
+import * as styles from './cta.module.css';
 
-const cta = ({ content = '' }) => {
+const cta = () => {
   return (
     <div>
       <div className={styles.list}>
         <a
-          href="http://www.govio.be/doc/Technische%20fiche.pdf"
+          href="http://www.govio.be/doc/technische-fiche.pdf"
           target="_blank"
         >
           <div className={`${styles.block} ${styles.block__1}`}>
@@ -15,14 +15,14 @@ const cta = ({ content = '' }) => {
           </div>
         </a>
         <a
-          href="http://www.govio.be/doc/Algemene%20voorwaarden.pdf"
+          href="http://www.govio.be/doc/algemene-voorwaarden.pdf"
           target="_blank"
         >
           <div className={`${styles.block} ${styles.block__2}`}>
             Algemene Voorwaarden
           </div>
         </a>
-        <a href="http://www.govio.be/doc/Handleiding.pdf" target="_blank">
+        <a href="http://www.govio.be/doc/handleiding.pdf" target="_blank">
           <div className={styles.block}>Volledige Handleiding</div>
         </a>
       </div>
